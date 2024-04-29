f6e83e1951b2373e77ff538030fc5c31def4f547
Justin Fiset <justinfiset@hotmail.fr>
2024-04-29 10:32:50 -0400

    Fun with the shaders

--- ElevateEngine/src/ElevateEngine/Core/Application.cpp
+++ ElevateEngine/src/ElevateEngine/Core/Application.cpp
@@ -49,9 +49,12 @@ namespace Elevate {
 			
 			layout(location = 0) in vec3 a_Position;
 
+			out vec3 o_Position;
+
 			void main()
 			{
-				gl_Position = vec4(a_Position - 0.5f, 1.0);
+				gl_Position = vec4(a_Position, 1.0);
+				o_Position = a_Position * 2 + 0.5;
 			}	
 		)";
 
@@ -59,10 +62,12 @@ namespace Elevate {
 			#version 330 core
 			
 			layout(location = 0) out vec4 o_Color;
+			in vec3 o_Position;
 
 			void main()
 			{
-				o_Color = vec4(0.8, 0.2, 0.3, 1.0);
+				//o_Color = vec4(0.8, 0.2, 0.3, 1.0);
+				o_Color = vec4(o_Position.xyz, 1.0);
 			}	
 		)";
 
