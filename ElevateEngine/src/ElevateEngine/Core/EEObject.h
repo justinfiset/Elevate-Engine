d082bebc5fdb35a21c40df45380987c18172bb19
Justin Fiset <justinfiset@hotmail.fr>
2026-04-26 16:36:20 -0400

    EEObject default constructor declaration

--- ElevateEngine/src/ElevateEngine/Core/EEObject.h
+++ ElevateEngine/src/ElevateEngine/Core/EEObject.h
@@ -119,6 +119,7 @@ namespace Elevate
 		Guid m_guid;
 
 	public:
+		EEObject() = default;
 		EEObject(EECategory category)
 			: m_category(category), m_guid(Guid::New()) { }
 		virtual ~EEObject() = default;
