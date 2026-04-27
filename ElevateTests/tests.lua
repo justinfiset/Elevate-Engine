f640060a54db28d167df797675aac68712e7783b
Justin Fiset <justinfiset@hotmail.fr>
2026-04-27 13:00:04 -0400

    Simple EEObjectPtr serialization test

--- ElevateTests/tests.lua
+++ ElevateTests/tests.lua
@@ -1,9 +1,15 @@
 vendorPath = path.getabsolute(directory.."/../Vendor")
 catchInclude = path.getabsolute(vendorPath.."/Catch2/src/")
-filter {}
-    includedirs
-    {
-        vendorPath.."/_config_headers/", 
-        catchInclude 
-    }
+
+files
+{
+    catchInclude .. "/**/*.cpp"
+}
+
+
+includedirs
+{
+    vendorPath.."/_config_headers/", 
+    catchInclude 
+}
 Logger.Info("Added Catch2 include directory: " .. catchInclude)
