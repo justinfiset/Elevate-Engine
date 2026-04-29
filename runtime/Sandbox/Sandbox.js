f7a9235fdfd49a6cc51fd872e88b1ce640d91480
justinfiset <justinfiset@users.noreply.github.com>
2026-04-29 03:31:48 +0000

    Deploying to gh-pages from @ justinfiset/Elevate-Engine@569703a16468550340996c09ab3f52e4af15909e 🚀

--- runtime/Sandbox/Sandbox.js
+++ runtime/Sandbox/Sandbox.js
@@ -85,7 +85,7 @@ if (ENVIRONMENT_IS_NODE) {
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: C:\Users\RUNNER~1\AppData\Local\Temp\tmp5rr3fme1.js
+// include: C:\Users\RUNNER~1\AppData\Local\Temp\tmpgiyylj73.js
 if (!Module["expectedDataFileDownloads"]) Module["expectedDataFileDownloads"] = 0;
 
 Module["expectedDataFileDownloads"]++;
@@ -757,23 +757,23 @@ Module["expectedDataFileDownloads"]++;
   });
 })();
 
-// end include: C:\Users\RUNNER~1\AppData\Local\Temp\tmp5rr3fme1.js
-// include: C:\Users\RUNNER~1\AppData\Local\Temp\tmpy7k19sbt.js
+// end include: C:\Users\RUNNER~1\AppData\Local\Temp\tmpgiyylj73.js
+// include: C:\Users\RUNNER~1\AppData\Local\Temp\tmphia4z0xr.js
 // All the pre-js content up to here must remain later on, we need to run
 // it.
 if ((typeof ENVIRONMENT_IS_WASM_WORKER != "undefined" && ENVIRONMENT_IS_WASM_WORKER) || (typeof ENVIRONMENT_IS_PTHREAD != "undefined" && ENVIRONMENT_IS_PTHREAD) || (typeof ENVIRONMENT_IS_AUDIO_WORKLET != "undefined" && ENVIRONMENT_IS_AUDIO_WORKLET)) Module["preRun"] = [];
 
 var necessaryPreJSTasks = Module["preRun"].slice();
 
-// end include: C:\Users\RUNNER~1\AppData\Local\Temp\tmpy7k19sbt.js
-// include: C:\Users\RUNNER~1\AppData\Local\Temp\tmpb0fz2k9k.js
+// end include: C:\Users\RUNNER~1\AppData\Local\Temp\tmphia4z0xr.js
+// include: C:\Users\RUNNER~1\AppData\Local\Temp\tmp6u28ocfl.js
 if (!Module["preRun"]) throw "Module.preRun should exist because file support used it; did a pre-js delete it?";
 
 necessaryPreJSTasks.forEach(task => {
   if (Module["preRun"].indexOf(task) < 0) throw "All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?";
 });
 
-// end include: C:\Users\RUNNER~1\AppData\Local\Temp\tmpb0fz2k9k.js
+// end include: C:\Users\RUNNER~1\AppData\Local\Temp\tmp6u28ocfl.js
 var arguments_ = [];
 
 var thisProgram = "./this.program";
