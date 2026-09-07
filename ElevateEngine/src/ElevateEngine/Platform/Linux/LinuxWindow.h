46be7f1d4f8146d7da16411d088bf8c2fd0786b6
Justin Fiset <justinfiset@hotmail.fr>
2026-09-07 11:32:34 -0400

    Fix builds

--- ElevateEngine/src/ElevateEngine/Platform/Linux/LinuxWindow.h
+++ ElevateEngine/src/ElevateEngine/Platform/Linux/LinuxWindow.h
@@ -1,6 +1,6 @@
 #pragma once
 #ifdef EE_PLATFORM_LINUX
-#include <Platform/Shared/GLFW/GlfwWindow.h>
+#include <ElevateEngine/Platform/Shared/GLFW/GlfwWindow.h>
 
 namespace Elevate
 {
--- ElevateEngine/src/ElevateEngine/Platform/Web/WebInput.h
+++ ElevateEngine/src/ElevateEngine/Platform/Web/WebInput.h
@@ -1,6 +1,6 @@
 #pragma once
 #ifdef EE_PLATFORM_WEB
-#include <Platform/Shared/GLFW/GlfwInput.h>
+#include <ElevateEngine/Platform/Shared/GLFW/GlfwInput.h>
 
 namespace Elevate
 {
